467c8dc75b1061c1e90273db4f3422e09eb35c2a
harry <langzhi@jiankongbao.com>
2015-11-27 16:03:12 +0800

    更改dao继承方式

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//var UserDao = require('./dao/user');
+var UserDao = require('./dao/user');
 
 /*UserDao.save({
  username:'harry.lang',
@@ -13,17 +13,18 @@
  console.log(require('validator').isEmail('aha@s.com'))
  console.log('----------------')*/
 
-/*UserDao.findOne({'$or': [
- {'username': 'harry.'},
- {'email': 'harry.lang@yunzhihui.com'}
- ]}, function (error, data) {
- console.log(arguments)
- });*/
-var mailhHtml = '<div>您已注册成功，请点击以下链接完成验证：</div><a href="www.learnjs.cn">go</a>';
-require('./helpers/send_email')('harry.lang@yunzhihui.com', 'ant用户注册验证', mailhHtml, function (error, info) {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('Message sent: ' + info.response);
-    }
+UserDao.findOne({'$or': [
+    {'username': 'harry.'},
+    {'email': 'harry.lang@yunzhihui.com'}
+]}, function (error, data) {
+    console.log(arguments)
 });
+/*
+ var mailhHtml = '<div>您已注册成功，请点击以下链接完成验证：</div><a href="www.learnjs.cn">go</a>';
+ require('./helpers/send_email')('harry.lang@yunzhihui.com', 'ant用户注册验证', mailhHtml, function (error, info) {
+ if (error) {
+ console.log(error);
+ } else {
+ console.log('Message sent: ' + info.response);
+ }
+ });*/
